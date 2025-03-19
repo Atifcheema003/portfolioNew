d219ae97ed1a6a67316a464bddbbe4ca0fb837af
atifcheema003 <atifcheema003@gmail.com>
2025-03-19 21:58:23 +0500

    Updated portfolio with new changes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    
     const mobileMenu = document.getElementById('mobile-menu');
     const navbarMenu = document.querySelector('.navbar-menu');
 
